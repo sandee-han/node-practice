32191c35860a12a8c465d768a7d258fc9fb2f7a7
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2023-12-13 11:53:01 +0900

    FEAT: add new todo at MongoDB 1. add new todo at MongoDB through add.html 2. make dateFormat for todo (YYYY/MM/DD)

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const app = express();
 
 // 포트번호
 var portNumber = 8088;
-
-
 app.listen(portNumber, () => console.log(`${portNumber}포트입니다.`));
+
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 
 // 환경변수 불러오기
 dotenv.config();
-
+ 
 var mongodbUser = process.env.DB_USER;
 var mongodbPassword = process.env.DB_PASSWORD;
 
@@ -44,20 +44,39 @@ mongoose
         console.log(err);
     });
 
+
+// todo DB Schema 생성
 const todoSchema = new mongoose.Schema({
     title: String,
-    content: Number,
+    content: String,
     dueDate: String,
     date: String
 });
 
-const Todo = mongoose.model('Todo', todoSchema);
+const todo = mongoose.model('todo', todoSchema);
 
-// async function saveMovie() {
-//     const amadeus = new tidi({title : 'Amadeus', year: 1986, score: 9.2, rating:'R'})
-//     amadeus.save()
-//     console.log(amadeus)    
-// }
+// 입력된 todo DB 저장
+async function saveTodo(request) {
+    const firstTodo = new todo(
+        {title : request.body.title, 
+        content : request.body.content,
+        dueDate : request.body.dueDate,
+        date : dateFormat(today)});
+    firstTodo.save();
+    console.log(firstTodo);
+}
+
+// 날짜
+const today = new Date;
+
+function dateFormat(today) {
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var date = today.getDate();
+
+    return year + '/' + month + '/' + date;
+}
+
 
 app.get('/pet', function (request, response) {
     response.send('shopping page for pet item');
@@ -68,13 +87,14 @@ app.get('/beauty', function (request, response) {
 });
 
 app.get('/add', function (request, response) {
-    response.sendfile(__dirname + '/add.html')
-    console.log('/add접속!')
+    response.sendfile(__dirname + '/src/board/etc/add.html');
+    console.log('/add접속!');
 });
 
 app.post('/add', function(request, response){
-    console.log(request.body)
-    response.send(request.body)
+    console.log(request.body);
+    saveTodo(request);
+    response.send(request.body);
 });
 
 app.get('/', function (request, response) {
